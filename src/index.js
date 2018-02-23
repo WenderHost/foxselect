6672980f56a735eee54e64f97e7d26b0fabc4a52
Michael Wender <michael@michaelwender.com>
2018-02-23 14:27:00 -0500

    Removing reference to `react-router-dom`

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { unregister } from './registerServiceWorker';
 
 import './css/style.css';
 import App from './App';
-import MhzCrystalSMD from './components/mhz-crystal-smd';
-import NotFound from './components/NotFound';
 
 const Root = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/mhz-crystal-smd" component={MhzCrystalSMD} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <App />
+    </div>
   );
 }
 
