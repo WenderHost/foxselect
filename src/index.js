7add8b85e419ea204a7dd92473622dcdc2e7e910
Michael Wender <michael@michaelwender.com>
2018-05-18 09:20:02 -0400

    Adding `core-js` to support Array.includes in IE 11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { unregister } from './registerServiceWorker';
+import 'core-js/es7/array';
 
 import './css/style.css';
 import App from './App';
