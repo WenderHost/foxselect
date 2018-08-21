60ba7e6137f0605b234ee0b288ce05ac0e4b9a82
Michael Wender <michael@michaelwender.com>
2018-08-21 08:33:02 -0400

    API_ROOT for SalesForce env

--- src/api-config.js
+++ src/api-config.js
@@ -1,14 +1,26 @@
 let apiHost;
+let apiRoot;
 const apiVersion = 'v1';
-
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname === 'foxonline.wenderhost.com'){
-  apiHost = 'https://foxonline.wenderhost.com/'
-} else if(hostname === 'foxelectronics.loco'){
-  apiHost = 'http://foxelectronics.loco'
-} else {
-  apiHost = 'http://foxelectronics.loco'
+switch( hostname ){
+  case 'foxonline.wenderhost.com':
+  case 'foxonline--foxselect.lightning.force.com':
+    apiHost = 'https://foxonline.wenderhost.com/'
+    break
+
+  case 'foxselect.ngrok.io':
+    apiHost = 'https://foxwebsite.ngrok.io/'
+    break
+
+  default:
+    apiHost = 'http://foxelectronics.loco/'
 }
 
-export const API_ROOT = `${apiHost}/wp-json/foxparts/${apiVersion}/get_options/`;
+if( -1 < apiHost.indexOf( 'force' ) ){
+  apiRoot = `${apiHost}services/apexrest/PartService/`
+} else {
+  apiRoot = `${apiHost}wp-json/foxparts/${apiVersion}/get_options/`
+}
+
+export const API_ROOT = apiRoot;
