2799aca0df7ec30a4379b6bab6734dc52c4302b4
Michael Wender <michael@michaelwender.com>
2018-12-13 10:15:08 -0500

    Adding namespaced Bootstrap CSS

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react' // , { Component }
 import { render } from 'react-dom'
 import { unregister } from './registerServiceWorker'
 import 'core-js/es7/array'
 
+import './css/bootstrap-ns.css'
 import './css/style.css'
 import App from './App'
+import { API_ENV } from './api-config';
 
-class Root extends Component{
-  constructor(){
-    super()
-
-    this.state = {
-      dataService: {}
-    }
-  }
-
-  passDataService( dataService ){
-    this.setState({dataService: dataService})
-  }
-
-  render(){
-    const { dataService } = this.state;
-
-    return(
-      <div>
-        <App dataService={dataService} />
-      </div>
-    )
-  }
+/**
+ * Setup our initialization method.
+ *
+ * When we're loading directly inside a browser (i.e. API_ENV == `web`),
+ * we simply call `init()` directly. When API_ENV == `salesforce`, we
+ * provide the `init` method via a global `FoxSelect` object which
+ * can then be accessed inside the Salesforce environment.
+ *
+ * @param      object  el       The DOM object we're attaching to
+ * @param      object  service  Connection for passing the Part No. back to Salesforce
+ */
+export var init = function(el, service){
+  render((
+    <App dataService={service} />
+    ),el)
 }
-render(<Root ref={(FoxSelect) => {window.FoxSelect = FoxSelect}} />,document.querySelector('#root'))
 
-//registerServiceWorker()
+if( 'web' === API_ENV ){
+  init(document.querySelector('#root'),null)
+} else {
+  var FoxSelect = {}
+  FoxSelect.init = init;
+  window.FoxSelect = FoxSelect;
+}
+
 unregister()
